6af9976f70b0323e85db6e1421c64c3681aa7776
puny97 <puneet.kamatagi999@gmail.com>
2026-07-19 17:07:44 +0530

    Fixed navbar

--- app/components/login/GlobalLogin.tsx
+++ app/components/login/GlobalLogin.tsx
@@ -1,19 +1,19 @@
 'use client'
 
+import { LuChartCandlestick } from 'react-icons/lu'
 import { MdLock } from 'react-icons/md'
-import { SlEqualizer } from 'react-icons/sl'
 
 const GlobalLogin = () => {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen bg-gray-150">
       <div className="flex flex-col items-center justify-center border border-gray-300 rounded-lg shadow-lg bg-white pb-15 pt-10 px-10">
-        <div className="flex items-center justify-center mb-4 bg-[#2B57F5] rounded-lg p-3">
-          <SlEqualizer size={20} color="white" />
+        <div className="flex items-center justify-center mb-4 bg-accent rounded-lg p-3">
+          <LuChartCandlestick size={20} color="white" />
         </div>
         <h1 className="text-2xl font-bold mb-1">Trade Journal</h1>
         <h3 className="text-md text-gray-600 text-center">Track every trade, day by day.</h3>
         <button
-          className="flex items-center justify-center gap-2 mt-6 px-4 py-2 bg-[#2B57F5] text-white rounded-lg scale-100 hover:bg-[#1A3DBF] hover:scale-110 transition-colors duration-300 cursor-pointer hover:shadow-lg hover:shadow-[#2B57F5]/50"
+          className="flex items-center justify-center gap-2 mt-6 px-4 py-2 bg-accent text-white rounded-lg scale-100 hover:bg-accent-foreground hover:scale-110 transition-colors duration-300 cursor-pointer hover:shadow-lg hover:shadow-accent/50"
           type="submit"
         >
           <MdLock />
